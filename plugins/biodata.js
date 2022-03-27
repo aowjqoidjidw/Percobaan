a0193b9a6d181fc0edd73b5e49f2d987c2bb4d9e
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-27 23:37:45 +0700

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -3,23 +3,23 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 *BIODATA OWNER*
 
-*Nama* : Raditya
-*Umur* : 19
-*Kelas* : 3 Smk
+*Nama* : rozi
+*Umur* : 15
+*Kelas* : SMPN 09
 *Status* : Private
 
 *SOSIAL MEDIA*
 
 *instagran* : Private
 *Facebook* : Private
-*Chanel Youtube* : none
-*Gmail* : Xnxx@gmail.com
-*Github* : https://github.com/Drz103
+*Chanel Youtube* : zifabotz
+*Gmail* : roziganteng559@gmail.com
+*Github* : https://github.com/amrozi
 
 _Oke udah itu aja terimakasih_
 
 `.trim()
-conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Raditya', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By rozi', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
 
 }
 handler.help = ['infoowner', 'infocreator']
